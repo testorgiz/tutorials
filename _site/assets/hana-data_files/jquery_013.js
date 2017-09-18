098d18de196f78678bfbce38a239ebaebd2b4d23
Lalit Pareek <lalit.pareek@sap.com>
2017-09-18 19:44:17 -0400

    cleanup and add validation sample

--- _site/assets/hana-data_files/jquery_013.js
+++
@@ -1,7 +0,0 @@
-// init link forward
-jQuery(function(){
-	jQuery('a[href="#"]').not('.opener, .nav-opener, li.has-drop>a, .btn-next, .btn-prev, .close').on('click', function(e){
-		location.href = "backPage.html";
-		e.preventDefault();
-	});
-});
